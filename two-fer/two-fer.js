2673f81b8904c702762a506c11c98aa41f05dd04
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-03-28 18:28:31 -0500

    Complete first two exercises

---
+++ two-fer/two-fer.js
@@ -0,0 +1,10 @@
+
+const twoFer = (name) => {
+    if (name === undefined ) {
+        return `One for you, one for me.`
+    } else {
+        return `One for ${name}, one for me.`
+    }
+}
+
+export {twoFer}

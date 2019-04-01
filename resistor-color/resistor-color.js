50bd557a17a5eacfdea7fa4dda742240f32f001f
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-03-31 22:04:13 -0500

    Add instructions in the comments of each exercise

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,3 +1,4 @@
+// Resistors have color coded bands, where each color maps to a number. The first 2 bands of a resistor have a simple encoding scheme: each color maps to a single number. Mnemonics map the colors to the numbers, that, when stored as an array, happen to map to their index in the array: Better Be Right Or Your Great Big Values Go Wrong.
 
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
 

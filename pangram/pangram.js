831617d68fb4700e3e8f6170ad3009cefbc19554
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-04-03 21:00:37 -0500

    Update solution to make alphabet from string and use .every method found on community solutions

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,18 +1,8 @@
-
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
 
 const isPangram = (sentence) => {
-    let found = false
     sentence = sentence.toLowerCase()
-    for (let i=0; i < alphabet.length; i++) {
-        if(sentence.includes(alphabet[i])) {
-            found = true
-        } else {
-            found = false
-            break
-        }
-    }
-    return found
+    return alphabet.every(letter => sentence.includes(letter))
 }
 
 export {isPangram}
